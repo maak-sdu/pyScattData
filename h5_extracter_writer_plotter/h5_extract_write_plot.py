d1334bdc2c5b190c3f8587ca23e65a12c69ca9a5
Martin Karlsen <maak@sdu.dk>
2022-02-02 08:21:44 +0100

    updated h5_extracter_writer_plotter

--- h5_extracter_writer_plotter/h5_extract_write_plot.py
+++ h5_extracter_writer_plotter/h5_extract_write_plot.py
@@ -9,52 +9,43 @@ FIGSIZE = (12,4)
 FONTSIZE = 16
 YLABEL = r"$I$ $[\mathrm{counts}]$"
 
-# Billinge group colors
 COLOR_DICT = dict(BG_BLUE='#0B3C5D', BG_RED='#B82601',
                   BG_GREEN='#1c6b0a', BG_LIGHTBLUE='#328CC1',
                   BG_DARKBLUE='#062F4F', BG_YELLOW='#D9B310',
                   BG_DARKRED='#984B43', BG_BORDEAUX='#76323F',
                   BG_OLIVEGREEN='#626E60', BG_YELLOWREY='#AB987A',
                   BG_BROWNORANGE='#C09F80')
-
 COLOR = COLOR_DICT['BG_BLUE']
 
-def h5_extract_write_plot():
-    h5path = Path.cwd() / 'h5'
-    if not h5path.exists():
-        h5path.mkdir()
-        print(f"{90*'-'}\nA folder named 'h5' has just been made.\
-                \nPlease place your .h5 files here and run the script again.\
-                \n{90*'-'}")
-        sys.exit()
-    files = list((Path.cwd() / 'h5').glob('*.h5'))
-    if len(files) == 0:
-        print(f"{90*'-'}\nPlease place your .h5 files in the h5 folder and run the script again.\
-                \n{90*'-'}")
-        sys.exit()
-    print(f"{90*'-'}\nPlease provide the desired file extenstion for the summed data file. (e.g. '.xy')")
-    fileext = input()
-    print(f"{90*'-'}\nPlease provide the desired number of files to be sum to each .{fileext} file. (e.g. 0, 5, or 'all')")
-    subframes_to_sum = input()
-    print(f"{90*'-'}\nPlease state whether you want to plots (png and pdf) of your .{fileext} files. (y/n)")
-    plot_req = input()
-    print(f"{90*'-'}\nPlease state whether you want to verbose print to the terminal. (y/n)")
-    print_req = input()
+def h5_extract_write_plot(h5_files):
+    fileext = input(f"{80*'-'}\nPlease provide the desired file extenstion for "
+                    f"the summed data file.\n(e.g. '.xy'): ")
+    subframes_to_sum = input(f"\nPlease provide the desired number of files to "
+                             f"be sum to each {fileext} file.\n(e.g. 0, 5, or "
+                             "'all'): ")
+
+    plot_req = input(f"\nPlease state whether you want to plots "
+                     f"(.png and .pdf) of your {fileext} files.\n(y/n): ")
+
+    print_req = input(f"\nPlease state whether you want to verbose print to "
+                      f"the terminal. (y/n): ")
     if plot_req == 'y' or plot_req == "'y'":
-        print(f"{90*'-'}\nPlease state the x quantity. ('twotheta' or 'q')")
-        xquantity = input()
+        xquantity = input(f"\nPlease state the x quantity. "
+                          f"('twotheta' or 'q'): ")
         if xquantity.lower() == 'q':
-            print(f"{90*'-'}\nPlease state the (reciprocal) x unit. ('nm' or 'angstrom')")
-            xunit = input()
+            xunit = input(f"\nPlease state the (reciprocal) x unit. "
+                          f"('nm' or 'angstrom'): ")
             if xunit.lower() == 'nm':
                 xlabel = r"$Q$ $[\mathrm{nm}^{-1}]$"
             elif xunit.lower() == 'angstrom':
                 xlabel = r"$Q$ $[\mathrm{\AA}^{-1}]$"
         elif xquantity.lower() == 'twotheta':
             xlabel = r"$2\theta$ $[\degree]$"
-        print(f"{90*'-'}\nSumming .h5 files, writing them to .{fileext} files, and plotting...")
+        print(f"{80*'-'}\nSumming .h5 files, writing them to .{fileext} files, "
+              f"and plotting...")
     else:
-        print(f"{90*'-'}\nSumming .h5 files, writing them to .{fileext} files...")
+        print(f"{80*'-'}\nSumming .h5 files, writing them to .{fileext} "
+              f"files...")
     if fileext[0] == "'" or fileext[0] == "\"":
         fileext = fileext[1:-1]
         fileext = fileext[1::]
@@ -66,7 +57,7 @@ def h5_extract_write_plot():
         if not folderpath.exists():
             folderpath.mkdir()
     skipped = []
-    for file in files:
+    for file in h5_files:
         f = h5py.File(file, 'r')
         fkeys = list(f.keys())
         d = {}
@@ -86,32 +77,41 @@ def h5_extract_write_plot():
             oom_scans_merged = int(np.log10(full_merges)) + 1
             oom_scans = int(np.log10(subframes)) + 1
             for i in range(full_merges):
-                scan_lower, scan_upper = 0+(i*subframes_to_sum), (subframes_to_sum-1)+(i*subframes_to_sum)
+                scan_lower = 0+(i*subframes_to_sum)
+                scan_upper = (subframes_to_sum-1)+(i*subframes_to_sum)
                 y = np.sum(d[fkeys[0]][scan_lower:scan_upper], axis=0)
-                scan_lower, scan_upper = str(scan_lower).zfill(oom_scans), str(scan_upper).zfill(oom_scans)
+                scan_lower = str(scan_lower).zfill(oom_scans)
+                scan_upper = str(scan_upper).zfill(oom_scans)
                 i_oom = str(i).zfill(oom_scans_merged)
                 if print_req == 'y':
                     print(f"\t\t{file.stem}_{i_oom}_{scan_lower}-{scan_upper}")
                 xy = np.column_stack((x, y))
-                np.savetxt(f'{fileext}/{file.stem}_{i_oom}_{scan_lower}-{scan_upper}.{fileext}', xy, fmt='%.10e')
+                np.savetxt(f'{fileext}/{file.stem}_{i_oom}_{scan_lower}-'
+                           f'{scan_upper}.{fileext}', xy, fmt='%.10e')
                 if plot_req == 'y':
                     fig = h5_to_xy_figure(x, y, xlabel)
-                    plt.savefig(f"png/{file.stem}_{i_oom}_{scan_lower}-{scan_upper}.png", bbox_inches='tight')
-                    plt.savefig(f"pdf/{file.stem}_{i_oom}_{scan_lower}-{scan_upper}.pdf", bbox_inches='tight')
+                    plt.savefig(f"png/{file.stem}_{i_oom}_{scan_lower}-"
+                                f"{scan_upper}.png", bbox_inches='tight')
+                    plt.savefig(f"pdf/{file.stem}_{i_oom}_{scan_lower}-"
+                                f"{scan_upper}.pdf", bbox_inches='tight')
                     plt.close()
             scan_lower, scan_upper = subframes - remainder, subframes
             y = np.sum(d[fkeys[0]][scan_lower:scan_upper], axis=0)
-            scan_lower, scan_upper = str(scan_lower).zfill(oom_scans), str(scan_upper).zfill(oom_scans)
+            scan_lower = str(scan_lower).zfill(oom_scans)
+            scan_upper = str(scan_upper).zfill(oom_scans)
             i = i + 1
             i_oom = str(i).zfill(oom_scans_merged)
             if print_req == 'y':
                 print(f"\t\t{file.stem}_{i_oom}_{scan_lower}-{scan_upper}")
             xy = np.column_stack((x, y))
-            np.savetxt(f'{fileext}/{file.stem}_{i_oom}_{scan_lower}-{scan_upper}.{fileext}', xy, fmt='%.10e')
+            np.savetxt(f'{fileext}/{file.stem}_{i_oom}_{scan_lower}-'
+                       f'{scan_upper}.{fileext}', xy, fmt='%.10e')
             if plot_req == 'y':
                 fig = h5_to_xy_figure(x, y, xlabel)
-                plt.savefig(f"png/{file.stem}_{i_oom}_{scan_lower}-{scan_upper}.png", bbox_inches='tight')
-                plt.savefig(f"pdf/{file.stem}_{i_oom}_{scan_lower}-{scan_upper}.pdf", bbox_inches='tight')
+                plt.savefig(f"png/{file.stem}_{i_oom}_{scan_lower}-{scan_upper}"
+                            f".png", bbox_inches='tight')
+                plt.savefig(f"pdf/{file.stem}_{i_oom}_{scan_lower}-{scan_upper}"
+                            f".pdf", bbox_inches='tight')
                 plt.close()
         elif subframes_to_sum == '0' or subframes_to_sum == '1':
             for i in range(d[fkeys[0]].shape[0]):
@@ -121,32 +121,43 @@ def h5_extract_write_plot():
                 i_oom = str(i).zfill(oom_scans)
                 if print_req == 'y':
                     print(f"\t\t{file.stem}_{i_oom}")
-                np.savetxt(f'{fileext}/{file.stem}_{i_oom}.{fileext}', xy, fmt='%.10e')
+                np.savetxt(f'{fileext}/{file.stem}_{i_oom}.{fileext}',
+                           xy, fmt='%.10e')
                 if plot_req == 'y':
                     fig = h5_to_xy_figure(x, y, xlabel)
-                    plt.savefig(f"png/{file.stem}_{i_oom}.png", bbox_inches='tight')
-                    plt.savefig(f"pdf/{file.stem}_{i_oom}.pdf", bbox_inches='tight')
+                    plt.savefig(f"png/{file.stem}_{i_oom}.png",
+                                bbox_inches='tight')
+                    plt.savefig(f"pdf/{file.stem}_{i_oom}.pdf",
+                                bbox_inches='tight')
                     plt.close()
         elif subframes_to_sum == "all" or subframes_to_sum == "'all'":
             y = np.sum(d[fkeys[0]], axis=0)
             xy = np.column_stack((x, y))
             oom = int(np.log10(d[fkeys[0]].shape[0])) + 1
-            scan_lower, scan_upper = str(0).zfill(oom), str(d[fkeys[0]].shape[0]).zfill(oom)
+            scan_lower = str(0).zfill(oom)
+            scan_upper = str(d[fkeys[0]].shape[0]).zfill(oom)
             if print_req == 'y':
                 print(f"\t\t{file.stem}_{scan_lower}-{scan_upper}")
-            np.savetxt(f'{fileext}/{file.stem}_{scan_lower}-{scan_upper}.{fileext}', xy, fmt='%.10e')
+            np.savetxt(f'{fileext}/{file.stem}_{scan_lower}-{scan_upper}.'
+                       f'{fileext}', xy, fmt='%.10e')
             if plot_req == 'y':
                 fig = h5_to_xy_figure(x, y, xlabel)
-                plt.savefig(f"png/{file.stem}_{scan_lower}-{scan_upper}.png", bbox_inches='tight')
-                plt.savefig(f"pdf/{file.stem}_{scan_lower}-{scan_upper}.pdf", bbox_inches='tight')
+                plt.savefig(f"png/{file.stem}_{scan_lower}-{scan_upper}.png",
+                            bbox_inches='tight')
+                plt.savefig(f"pdf/{file.stem}_{scan_lower}-{scan_upper}.pdf",
+                            bbox_inches='tight')
                 plt.close()
+    print("Done summing h5 files.")
     if len(skipped) != 0:
-        print(f"{90*'-'}\nLooks like one or more non-integrated .h5 files are present in the h5 folder:")
+        print(f"{80*'-'}\nATTENTION: Looks like one or more non-integrated .h5 "
+              f"files are present in the h5\nfolder:")
         for e in skipped:
             print(f"\t{e}")
-        print("\nThe abovementioned files were not summed as they need to be azimuthally integrated.")
-    print(f"{90*'-'}\n.{fileext} files have been written to the {fileext} folder.\
-            \nPlots have been saved to the png and pdf folders.\n{90*'-'}")
+        print("The abovementioned files were not summed as they need to be "
+              "azimuthally\nintegrated.")
+    print(f"{80*'-'}\n.{fileext} files have been written to the '{fileext}' "
+          f"folder.\nPlots have been saved to the 'png' and 'pdf' folders."
+          f"\n{80*'-'}")
 
     return None
 
@@ -163,7 +174,19 @@ def h5_to_xy_figure(x, y, xlabel):
 
 
 def main():
-    h5_extract_write_plot()
+    h5path = Path.cwd() / 'h5'
+    if not h5path.exists():
+        h5path.mkdir()
+        print(f"{80*'-'}\nA folder named 'h5' has just been made.\
+                \nPlease place your .h5 files here and run the script again.\
+                \n{80*'-'}")
+        sys.exit()
+    h5_files = list((Path.cwd() / 'h5').glob('*.h5'))
+    if len(h5_files) == 0:
+        print(f"{80*'-'}\nPlease place your .h5 files in the h5 folder and run "
+              f"the script again.\n{80*'-'}")
+        sys.exit()
+    h5_extract_write_plot(h5_files)
 
     return None
 
