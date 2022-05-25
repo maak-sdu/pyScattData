bdc0696ec6c54f2115772b7cb42850e156727874
Martin Aaskov Karlsen <maak@sdu.dk>
2022-05-25 11:10:28 +0200

    fixed Ewe label

--- xy_overview_stack_echem/example/gr_gr/xy_overview_stack_echem.py
+++ xy_overview_stack_echem/example/gr_gr/xy_overview_stack_echem.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 from diffpy.utils.parsers.loaddata import loadData
 import numpy as np
+from scipy.constants import physical_constants
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from matplotlib.gridspec import GridSpec
@@ -11,6 +12,21 @@ try:
     PLOT_STYLE = "found"
 except ModuleNotFoundError:
     PLOT_STYLE = None
+
+
+# Inputs to load echem
+INDEX_TIME = 0
+INDEX_VOLTAGE = 1
+INDEX_CURRENT = 2
+
+# Inputs to calculate amount of working ion transferred
+WORKING_ION_CHARGE = 1
+WORKING_ION_START_VALUE = 0
+MOLAR_MASS = 79.866
+MASS = 0.6 * 11.276 * 10**-3
+
+TOLERANCE_FACTOR = 10**2
+VLINES_ECHEM = True
 
 
 SCATTLABEL_DICT = {"gr": dict(x = r"$r$ $[\mathrm{\AA}]$",
@@ -37,7 +53,13 @@ ECHEMLABEL_DICT = {"V_t[h]": dict(x = r"$t$ $[\mathrm{h}]$",
                    "Ewe_Li_t[h]": dict(x = r"$t$ $[\mathrm{h}]$",
                                        y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Li/Li$^{+} [\mathrm{V}]$"),
                    "Ewe_Na_t[h]": dict(x = r"$t$ $[\mathrm{h}]$",
-                                       y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Na/Na$^{+} [\mathrm{V}]$")
+                                       y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Na/Na$^{+} [\mathrm{V}]$"),
+                   "V_x[h]": dict(x = r"$x$ in Li$_{x}$TiO$_{2}$",
+                                  y = r"$V$ $[\mathrm{V}]$"),
+                   "Ewe_Li_x[h]": dict(x = r"$x$ in Li$_{x}$TiO$_{2}$",
+                                       y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Li/Li$^{+}$ $[\mathrm{V}]$"),
+                   "Ewe_Na_t[h]": dict(x = r"$x$ in Li$_{x}$TiO$_{2}$",
+                                       y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Na/Na$^{+}$ $[\mathrm{V}]$")
                     }
 
 MY_GRADIENT = LinearSegmentedColormap.from_list('my_gradient', (
@@ -67,7 +89,8 @@ PLOT_DICT = dict(dpi = 600,
                  xlabel_lower = SCATTLABEL_DICT["gr"]["x"],
                  ylabel_lower = SCATTLABEL_DICT["gr"]["y"],
                  timelabel = ECHEMLABEL_DICT["Ewe_Li_t[h]"]["x"],
-                 voltagelabel = ECHEMLABEL_DICT["Ewe_Li_t[h]"]["y"],
+                 voltagelabel = ECHEMLABEL_DICT["Ewe_Li_x[h]"]["y"],
+                 xlabel = ECHEMLABEL_DICT["Ewe_Li_x[h]"]["x"],
                  minor_tick_index_upper = 5,
                  minor_tick_index_lower = 5,
                  minor_tick_index_echem = 5,
@@ -75,12 +98,12 @@ PLOT_DICT = dict(dpi = 600,
                  cmap_lower = MY_GRADIENT,
                  cbar_ticks_upper = 5,
                  cbar_ticks_lower = 5,
-                 color = "k",
-                 linewidth = 1,
+                 linewidth = 2,
                  hspace = 0.1)
 
 
 def xy_stack(data_files):
+    d = {}
     for i in range(len(data_files)):
         print(f"\t{data_files[i].name}")
         xy = loadData(str(data_files[i]))
@@ -89,20 +112,65 @@ def xy_stack(data_files):
             y_array = y
         else:
             y_array = np.column_stack((y_array, y))
+    d["x"], d["y_array"] = x, y_array
 
-    return x, y_array
+    return d
 
 
-def xy_overview_stack_echem(d_data, d_plot, output_paths):
-    x_upper, y_upper = d_data["x_upper"], d_data["y_upper"]
-    x_lower, y_lower = d_data["x_lower"], d_data["y_lower"]
-    time, voltage = d_data["time"], d_data["voltage"]
-    # xmin_upper, xmax_upper = np.amin(x_upper), np.amax(x_upper)
-    # ymin_upper, ymax_upper = np.amin(y_upper), np.amax(y_upper)
-    # xmin_lower, xmax_lower = np.amin(x_lower), np.amax(x_lower)
-    # ymin_lower, ymax_lower = np.amin(y_lower), np.amax(y_lower)
+def echem_load(echem_file):
+    d = {}
+    data = loadData(str(echem_file))
+    d["time"], d["voltage"] = data[:,INDEX_TIME], data[:,INDEX_VOLTAGE]
+    d["current"] = data[:,INDEX_CURRENT]
+
+    return d
+
+
+def x_from_dict_calculate(d):
+    time, current = d["time"], d["current"]
+    x = [WORKING_ION_START_VALUE]
+    n = MASS / MOLAR_MASS
+    f = physical_constants["Faraday constant"][0]
+    for i in range(1, len(time)):
+        delta_q =  - current[i] * (time[i] - time[i-1]) * 60**2
+        delta_x = delta_q / (n * f)
+        x.append(x[i-1] + delta_x)
+    change_indices = [i for i in range(1, len(current))
+                      if current[i] != 0
+                      and current[i] * current[i-1] <= 0]
+    d["x"], d["change_indices"] = np.array(x), np.array(change_indices)
+
+    return d
+
+
+def xy_overview_stack_echem(d_upper, d_lower, d_echem, d_plot, output_paths):
+    x_upper, y_upper = d_upper["x"], d_upper["y_array"]
+    x_lower, y_lower = d_lower["x"], d_lower["y_array"]
+    time, voltage = d_echem["time"], d_echem["voltage"]
+    current, x = d_echem["current"], d_echem["x"]
+    change_indices = d_echem["change_indices"]
+    t_changes = [time[e] for e in change_indices]
+    t_changes_labels = [f"{x[e]:.2f}" for e in change_indices]
+    xticks_labels = [f"{e:.1f}" for e in np.arange(0, 0.8, 0.2)]
+    xticks_labels.append(t_changes_labels[0])
+    for e in np.arange(0.6, 0.3, -0.2):
+        xticks_labels.append(f"{e:.1f}")
+    xticks_labels.append(t_changes_labels[1])
+    for e in np.arange(0.4, 0.6, 0.2):
+        xticks_labels.append(f"{e:.1f}")
+    t_xticks = np.array([])
+    j = 0
+    for i in range(0, len(x)):
+        if np.isclose(np.array(xticks_labels[j], dtype=float),
+                      x[i],
+                      atol=abs(x[0] - x[1]) * TOLERANCE_FACTOR
+                      ):
+            t_xticks = np.append(t_xticks, time[i])
+            j += 1
+            if j == len(xticks_labels):
+                break
     time_min, time_max = np.amin(time), np.amax(time)
-    # voltage_min, voltage_max = np.amin(voltage), np.amax(voltage)
+    time_range = time_max - time_min
     xrange_upper = d_plot["xmax_upper"] - d_plot["xmin_upper"]
     yrange_upper = d_plot["ymax_upper"] - d_plot["ymin_upper"]
     xrange_lower = d_plot["xmax_lower"] - d_plot["xmin_lower"]
@@ -203,14 +271,14 @@ def xy_overview_stack_echem(d_data, d_plot, output_paths):
                           origin="upper",
                           vmin=d_plot["ymin_upper"],
                           vmax=d_plot["ymax_upper"],
-                          extent=(0, scans_upper, d_plot["xmax_upper"], d_plot["xmin_upper"]),
+                          extent=(time_min, time_max, d_plot["xmax_upper"], d_plot["xmin_upper"]),
                           cmap=d_plot["cmap_upper"])
     cbar_ticks_upper = np.linspace(d_plot["ymin_upper"], d_plot["ymax_upper"], d_plot["cbar_ticks_upper"])
     cbar_0 = ax0.figure.colorbar(im_upper, ax=ax0, ticks=cbar_ticks_upper)
     if d_plot["ymax_upper"] > 100:
         cbar_0.formatter.set_powerlimits((0, 0))
     cbar_0.set_label(label=d_plot["ylabel_upper"], size=d_plot["fontsize_labels"])
-    ax0.set_xlabel(d_plot["indexlabel"], fontsize=d_plot["fontsize_labels"])
+    ax0.set_xlabel(d_plot["timelabel"], fontsize=d_plot["fontsize_labels"])
     ax0.xaxis.set_label_position("top")
     ax0.tick_params(axis="x",
                     labelbottom=False,
@@ -227,7 +295,7 @@ def xy_overview_stack_echem(d_data, d_plot, output_paths):
                              origin="upper",
                              vmin=d_plot["ymin_lower"],
                              vmax=d_plot["ymax_lower"],
-                             extent=(0, scans_lower, d_plot["xmax_lower"], d_plot["xmin_lower"]),
+                             extent=(time_min, time_max, d_plot["xmax_lower"], d_plot["xmin_lower"]),
                              cmap=d_plot["cmap_lower"])
     cbar_ticks_lower = np.linspace(d_plot["ymin_lower"], d_plot["ymax_lower"], d_plot["cbar_ticks_lower"])
     cbar_1 = ax1.figure.colorbar(im_lower, ax=ax1, ticks=cbar_ticks_lower)
@@ -242,15 +310,36 @@ def xy_overview_stack_echem(d_data, d_plot, output_paths):
     ax1.xaxis.set_minor_locator(ticker.MultipleLocator(base_scan_lower / d_plot["minor_tick_index_lower"]))
     ax1.yaxis.set_major_locator(ticker.MultipleLocator(base_scatt_lower))
     ax1.yaxis.set_minor_locator(ticker.MultipleLocator(base_scatt_lower / d_plot["minor_tick_index_lower"]))
-    ax2.plot(time, voltage, c=d_plot["color"], lw=d_plot["linewidth"])
+    ax21 = ax2.twiny()
+    ax21.plot(time, voltage, lw=d_plot["linewidth"])
+    ax21.set_xlim(time_min, time_max)
     ax2.set_xlim(time_min, time_max)
+    ax21.set_ylim(d_plot["voltage_min"], d_plot["voltage_max"])
     ax2.set_ylim(d_plot["voltage_min"], d_plot["voltage_max"])
-    ax2.set_xlabel(d_plot["timelabel"], fontsize=d_plot["fontsize_labels"])
+    # ax21.set_xlabel(d_plot["timelabel"], fontsize=d_plot["fontsize_labels"])
+    # ax21.set_ylabel(d_plot["voltagelabel"], fontsize=d_plot["fontsize_labels"])
+    ax21.xaxis.set_major_locator(ticker.MultipleLocator(base_time))
+    ax21.xaxis.set_minor_locator(ticker.MultipleLocator(base_time / d_plot["minor_tick_index_echem"]))
+    ax21.yaxis.set_major_locator(ticker.MultipleLocator(base_voltage))
+    ax21.yaxis.set_minor_locator(ticker.MultipleLocator(base_voltage / d_plot["minor_tick_index_echem"]))
+    ax21.tick_params(axis="x",
+                    labelbottom=False,
+                    labeltop=False,
+                    # labelsize=d_plot["fontsize_ticks"]
+                    )
+    ax2.set_xticks(t_xticks)
+    ax2.set_xticklabels(xticks_labels)
+    ax2.set_xlabel(d_plot["xlabel"], fontsize=d_plot["fontsize_labels"])
     ax2.set_ylabel(d_plot["voltagelabel"], fontsize=d_plot["fontsize_labels"])
-    ax2.xaxis.set_major_locator(ticker.MultipleLocator(base_time))
-    ax2.xaxis.set_minor_locator(ticker.MultipleLocator(base_time / d_plot["minor_tick_index_echem"]))
-    ax2.yaxis.set_major_locator(ticker.MultipleLocator(base_voltage))
-    ax2.yaxis.set_minor_locator(ticker.MultipleLocator(base_voltage / d_plot["minor_tick_index_echem"]))
+    ax2.xaxis.set_tick_params(labelsize=d_plot["fontsize_ticks"])
+    ax2.yaxis.set_tick_params(labelsize=d_plot["fontsize_ticks"])
+    if not isinstance(VLINES_ECHEM, type(None)):
+        ax21.axvline(x=0.987*t_changes[0], ls="--", c="k", lw=2, zorder=1)
+        ax21.axvline(x=0.9925*t_changes[1], ls="--", c="k", lw=2, zorder=1)
+        # for t in t_changes:
+        #     ax11.axvline(x=t, ls="--", c="k", lw=2, zorder=1)
+    # scan_time = np.array([i * (time_range / (scatt_array.shape[1] - 1))
+    #                       for i in range(scatt_array.shape[1])])
     for p in output_paths:
         plt.savefig(p / f"xy_overview_stack_echem.{p.name}",
                     bbox_inches="tight")
@@ -322,19 +411,20 @@ def main():
               f"the program.")
         sys.exit()
     print(f"{80*'-'}\nStacking xy data for upper plot...")
-    x_upper, y_upper = xy_stack(data_upper_files)
+    d_upper = xy_stack(data_upper_files)
     print(f"{80*'-'}\nStacking xy data for lower plot...")
-    x_lower, y_lower = xy_stack(data_lower_files)
+    d_lower = xy_stack(data_lower_files)
     echem_file = data_echem_files[0]
     print(f"{80*'-'}\nCollecting electrochemical data...\n\t{echem_file.name}")
-    time_voltage = np.loadtxt(echem_file)
-    data_dict = dict(x_upper = x_upper,
-                     y_upper = y_upper,
-                     x_lower = x_lower,
-                     y_lower = y_lower,
-                     time = time_voltage[:,0],
-                     voltage = time_voltage[:,1]
-                     )
+    d_echem = echem_load(echem_file)
+    d_echem = x_from_dict_calculate(d_echem)
+    # data_dict = dict(x_upper = x_upper,
+    #                  y_upper = y_upper,
+    #                  x_lower = x_lower,
+    #                  y_lower = y_lower,
+    #                  time = time_voltage[:,0],
+    #                  voltage = time_voltage[:,1]
+    #                  )
     output_paths = [Path.cwd() / "pdf",
                     Path.cwd() / "png",
                     Path.cwd() / "svg"]
@@ -342,7 +432,7 @@ def main():
         if not p.exists():
             p.mkdir()
     print(f"{80*'-'}\nMaking plot with stacked overview plots and echem...")
-    xy_overview_stack_echem(data_dict, PLOT_DICT, output_paths)
+    xy_overview_stack_echem(d_upper, d_lower, d_echem, PLOT_DICT, output_paths)
     output_folders = [p.name for p in output_paths]
     print(f"Done plotting.\n{80*'-'}\nPlease see the {output_folders} folders.")
 
--- xy_overview_stack_echem/example/iq_fq/xy_overview_stack_echem.py
+++ xy_overview_stack_echem/example/iq_fq/xy_overview_stack_echem.py
@@ -2,10 +2,10 @@ import sys
 from pathlib import Path
 from diffpy.utils.parsers.loaddata import loadData
 import numpy as np
-import matplotlib
-from mpl_toolkits.axes_grid1 import AxesGrid
+from scipy.constants import physical_constants
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import matplotlib.colors as colors
 from matplotlib.gridspec import GridSpec
 from matplotlib.colors import LinearSegmentedColormap
 try:
@@ -69,14 +69,14 @@ def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
         cdict['blue'].append((si, b, b))
         cdict['alpha'].append((si, a, a))
 
-    newcmap = matplotlib.colors.LinearSegmentedColormap(name, cdict)
+    newcmap = colors.LinearSegmentedColormap(name, cdict)
     plt.register_cmap(cmap=newcmap)
 
     return newcmap
 
 
 def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
-    new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+    new_cmap = colors.LinearSegmentedColormap.from_list(
                'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name,
                                                    a=minval,
                                                    b=maxval
@@ -89,6 +89,22 @@ def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
 ORIG_CMAP = MY_GRADIENT
 TRUNCATE_CMAP_IQ = truncate_colormap(MY_GRADIENT, 0.5, 1)
 SHRUNK_CMAP_FQ = shiftedColorMap(ORIG_CMAP, start=-0.5, midpoint=0.0, stop=1.0, name='shrunk')
+
+
+# Inputs to load echem
+INDEX_TIME = 0
+INDEX_VOLTAGE = 1
+INDEX_CURRENT = 2
+
+# Inputs to calculate amount of working ion transferred
+WORKING_ION_CHARGE = 1
+WORKING_ION_START_VALUE = 0
+MOLAR_MASS = 79.866
+MASS = 0.6 * 11.276 * 10**-3
+
+TOLERANCE_FACTOR = 10**2
+VLINES_ECHEM = True
+
 
 SCATTLABEL_DICT = {"gr": dict(x = r"$r$ $[\mathrm{\AA}]$",
                               y = r"$G$ $[\mathrm{\AA}^{-2}]$"),
@@ -114,23 +130,22 @@ ECHEMLABEL_DICT = {"V_t[h]": dict(x = r"$t$ $[\mathrm{h}]$",
                    "Ewe_Li_t[h]": dict(x = r"$t$ $[\mathrm{h}]$",
                                        y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Li/Li$^{+} [\mathrm{V}]$"),
                    "Ewe_Na_t[h]": dict(x = r"$t$ $[\mathrm{h}]$",
-                                       y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Na/Na$^{+} [\mathrm{V}]$")
+                                       y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Na/Na$^{+} [\mathrm{V}]$"),
+                   "V_x[h]": dict(x = r"$x$ in Li$_{x}$TiO$_{2}$",
+                                  y = r"$V$ $[\mathrm{V}]$"),
+                   "Ewe_Li_x[h]": dict(x = r"$x$ in Li$_{x}$TiO$_{2}$",
+                                       y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Li/Li$^{+}$ $[\mathrm{V}]$"),
+                   "Ewe_Na_t[h]": dict(x = r"$x$ in Li$_{x}$TiO$_{2}$",
+                                       y = r"$E_{\mathrm{we}}$ vs." + "\n" + r"Na/Na$^{+}$ $[\mathrm{V}]$")
                     }
-CMAPS = {0:'viridis', 1:'plasma', 2:'inferno', 3:'magma', 4:'Greys',
-         5:'Purples', 6:'Blues', 7:'Greens', 8:'Oranges', 9:'Reds',
-         10: 'YlOrBr', 11:'YlOrRd', 12:'OrRd', 13:'PuRd', 14:'RdPu',
-         15:'BuPu', 16:'GnBu', 17:'PuBu', 18:'YlGnBu', 19:'PuBuGn',
-         20:'BuGn', 21:'YlGn', 22:'binary', 23:'gist_yarg', 24:'gist_gray',
-         25:'gray', 26:'bone', 27:'pink', 28:'spring', 29:'summer',
-         30:'autumn', 31:'winter', 32:'cool', 33:'Wistia', 34:'hot',
-         35:'afmhot', 36:'gist_heat', 37:'copper', 38:'PiYG', 39:'PRGn',
-         40:'BrBG', 41:'PuOr', 42:'RdGy', 43:'RdBu', 44:'RdYlBu',
-         45:'RdYlGn', 46:'Spectral', 47:'coolwarm', 48:'bwr', 49:'seismic',
-         50:'twilight', 51:'twilight_shifted', 52:'hsv', 53:'ocean',
-         54:'gist_earth', 55:'terrain', 56:'gist_stern', 57:'gnuplot',
-         58:'gnuplot2', 59:'CMRmap', 60:'cubehelix', 61:'brg',
-         62:'gist_rainbow', 63:'rainbow', 64:'jet', 65:'turbo',
-         66:'nipy_spectral', 67:'gist_ncar'}
+
+MY_GRADIENT = LinearSegmentedColormap.from_list('my_gradient', (
+                 # Edit this gradient at https://eltos.github.io/gradient/#0B3C5D-0B3C5D-FFFFFF-B82601-B82601
+                 (0.000, (0.043, 0.235, 0.365)),
+                 (0.250, (0.200, 0.400, 0.500)),
+                 (0.500, (1.000, 1.000, 1.000)),
+                 (0.750, (0.850, 0.200, 0.100)),
+                 (1.000, (0.722, 0.149, 0.004))))
 PLOT_DICT = dict(dpi = 600,
                  figsize = (8,10),
                  fontsize_labels = 20,
@@ -151,7 +166,8 @@ PLOT_DICT = dict(dpi = 600,
                  xlabel_lower = SCATTLABEL_DICT["fq[A^-1]"]["x"],
                  ylabel_lower = SCATTLABEL_DICT["fq[A^-1]"]["y"],
                  timelabel = ECHEMLABEL_DICT["Ewe_Li_t[h]"]["x"],
-                 voltagelabel = ECHEMLABEL_DICT["Ewe_Li_t[h]"]["y"],
+                 voltagelabel = ECHEMLABEL_DICT["Ewe_Li_x[h]"]["y"],
+                 xlabel = ECHEMLABEL_DICT["Ewe_Li_x[h]"]["x"],
                  minor_tick_index_upper = 5,
                  minor_tick_index_lower = 5,
                  minor_tick_index_echem = 5,
@@ -159,12 +175,12 @@ PLOT_DICT = dict(dpi = 600,
                  cmap_lower = SHRUNK_CMAP_FQ,
                  cbar_ticks_upper = 9,
                  cbar_ticks_lower = 7,
-                 color = "k",
-                 linewidth = 1,
+                 linewidth = 2,
                  hspace = 0.1)
 
 
 def xy_stack(data_files):
+    d = {}
     for i in range(len(data_files)):
         print(f"\t{data_files[i].name}")
         xy = loadData(str(data_files[i]))
@@ -173,20 +189,65 @@ def xy_stack(data_files):
             y_array = y
         else:
             y_array = np.column_stack((y_array, y))
+    d["x"], d["y_array"] = x, y_array
 
-    return x, y_array
+    return d
 
 
-def xy_overview_stack_echem(d_data, d_plot, output_paths):
-    x_upper, y_upper = d_data["x_upper"], d_data["y_upper"]
-    x_lower, y_lower = d_data["x_lower"], d_data["y_lower"]
-    time, voltage = d_data["time"], d_data["voltage"]
-    # xmin_upper, xmax_upper = np.amin(x_upper), np.amax(x_upper)
-    # ymin_upper, ymax_upper = np.amin(y_upper), np.amax(y_upper)
-    # xmin_lower, xmax_lower = np.amin(x_lower), np.amax(x_lower)
-    # ymin_lower, ymax_lower = np.amin(y_lower), np.amax(y_lower)
+def echem_load(echem_file):
+    d = {}
+    data = loadData(str(echem_file))
+    d["time"], d["voltage"] = data[:,INDEX_TIME], data[:,INDEX_VOLTAGE]
+    d["current"] = data[:,INDEX_CURRENT]
+
+    return d
+
+
+def x_from_dict_calculate(d):
+    time, current = d["time"], d["current"]
+    x = [WORKING_ION_START_VALUE]
+    n = MASS / MOLAR_MASS
+    f = physical_constants["Faraday constant"][0]
+    for i in range(1, len(time)):
+        delta_q =  - current[i] * (time[i] - time[i-1]) * 60**2
+        delta_x = delta_q / (n * f)
+        x.append(x[i-1] + delta_x)
+    change_indices = [i for i in range(1, len(current))
+                      if current[i] != 0
+                      and current[i] * current[i-1] <= 0]
+    d["x"], d["change_indices"] = np.array(x), np.array(change_indices)
+
+    return d
+
+
+def xy_overview_stack_echem(d_upper, d_lower, d_echem, d_plot, output_paths):
+    x_upper, y_upper = d_upper["x"], d_upper["y_array"]
+    x_lower, y_lower = d_lower["x"], d_lower["y_array"]
+    time, voltage = d_echem["time"], d_echem["voltage"]
+    current, x = d_echem["current"], d_echem["x"]
+    change_indices = d_echem["change_indices"]
+    t_changes = [time[e] for e in change_indices]
+    t_changes_labels = [f"{x[e]:.2f}" for e in change_indices]
+    xticks_labels = [f"{e:.1f}" for e in np.arange(0, 0.8, 0.2)]
+    xticks_labels.append(t_changes_labels[0])
+    for e in np.arange(0.6, 0.3, -0.2):
+        xticks_labels.append(f"{e:.1f}")
+    xticks_labels.append(t_changes_labels[1])
+    for e in np.arange(0.4, 0.6, 0.2):
+        xticks_labels.append(f"{e:.1f}")
+    t_xticks = np.array([])
+    j = 0
+    for i in range(0, len(x)):
+        if np.isclose(np.array(xticks_labels[j], dtype=float),
+                      x[i],
+                      atol=abs(x[0] - x[1]) * TOLERANCE_FACTOR
+                      ):
+            t_xticks = np.append(t_xticks, time[i])
+            j += 1
+            if j == len(xticks_labels):
+                break
     time_min, time_max = np.amin(time), np.amax(time)
-    # voltage_min, voltage_max = np.amin(voltage), np.amax(voltage)
+    time_range = time_max - time_min
     xrange_upper = d_plot["xmax_upper"] - d_plot["xmin_upper"]
     yrange_upper = d_plot["ymax_upper"] - d_plot["ymin_upper"]
     xrange_lower = d_plot["xmax_lower"] - d_plot["xmin_lower"]
@@ -237,7 +298,7 @@ def xy_overview_stack_echem(d_data, d_plot, output_paths):
         base_voltage = 0.1
     elif 0.5 < voltage_range <= 1:
         base_voltage = 0.2
-    elif 1 < voltage_range <= 3:
+    elif 1 < d_plot["voltage_max"] <= 4:
         base_voltage = 0.5
     else:
         base_voltage = 1
@@ -287,14 +348,14 @@ def xy_overview_stack_echem(d_data, d_plot, output_paths):
                           origin="upper",
                           vmin=d_plot["ymin_upper"],
                           vmax=d_plot["ymax_upper"],
-                          extent=(0, scans_upper, d_plot["xmax_upper"], d_plot["xmin_upper"]),
+                          extent=(time_min, time_max, d_plot["xmax_upper"], d_plot["xmin_upper"]),
                           cmap=d_plot["cmap_upper"])
     cbar_ticks_upper = np.linspace(d_plot["ymin_upper"], d_plot["ymax_upper"], d_plot["cbar_ticks_upper"])
     cbar_0 = ax0.figure.colorbar(im_upper, ax=ax0, ticks=cbar_ticks_upper)
     if d_plot["ymax_upper"] > 100:
         cbar_0.formatter.set_powerlimits((0, 0))
     cbar_0.set_label(label=d_plot["ylabel_upper"], size=d_plot["fontsize_labels"])
-    ax0.set_xlabel(d_plot["indexlabel"], fontsize=d_plot["fontsize_labels"])
+    ax0.set_xlabel(d_plot["timelabel"], fontsize=d_plot["fontsize_labels"])
     ax0.xaxis.set_label_position("top")
     ax0.tick_params(axis="x",
                     labelbottom=False,
@@ -311,7 +372,7 @@ def xy_overview_stack_echem(d_data, d_plot, output_paths):
                              origin="upper",
                              vmin=d_plot["ymin_lower"],
                              vmax=d_plot["ymax_lower"],
-                             extent=(0, scans_lower, d_plot["xmax_lower"], d_plot["xmin_lower"]),
+                             extent=(time_min, time_max, d_plot["xmax_lower"], d_plot["xmin_lower"]),
                              cmap=d_plot["cmap_lower"])
     cbar_ticks_lower = np.linspace(d_plot["ymin_lower"], d_plot["ymax_lower"], d_plot["cbar_ticks_lower"])
     cbar_1 = ax1.figure.colorbar(im_lower, ax=ax1, ticks=cbar_ticks_lower)
@@ -326,15 +387,36 @@ def xy_overview_stack_echem(d_data, d_plot, output_paths):
     ax1.xaxis.set_minor_locator(ticker.MultipleLocator(base_scan_lower / d_plot["minor_tick_index_lower"]))
     ax1.yaxis.set_major_locator(ticker.MultipleLocator(base_scatt_lower))
     ax1.yaxis.set_minor_locator(ticker.MultipleLocator(base_scatt_lower / d_plot["minor_tick_index_lower"]))
-    ax2.plot(time, voltage, c=d_plot["color"], lw=d_plot["linewidth"])
+    ax21 = ax2.twiny()
+    ax21.plot(time, voltage, lw=d_plot["linewidth"])
+    ax21.set_xlim(time_min, time_max)
     ax2.set_xlim(time_min, time_max)
+    ax21.set_ylim(d_plot["voltage_min"], d_plot["voltage_max"])
     ax2.set_ylim(d_plot["voltage_min"], d_plot["voltage_max"])
-    ax2.set_xlabel(d_plot["timelabel"], fontsize=d_plot["fontsize_labels"])
+    # ax21.set_xlabel(d_plot["timelabel"], fontsize=d_plot["fontsize_labels"])
+    # ax21.set_ylabel(d_plot["voltagelabel"], fontsize=d_plot["fontsize_labels"])
+    ax21.xaxis.set_major_locator(ticker.MultipleLocator(base_time))
+    ax21.xaxis.set_minor_locator(ticker.MultipleLocator(base_time / d_plot["minor_tick_index_echem"]))
+    ax21.yaxis.set_major_locator(ticker.MultipleLocator(base_voltage))
+    ax21.yaxis.set_minor_locator(ticker.MultipleLocator(base_voltage / d_plot["minor_tick_index_echem"]))
+    ax21.tick_params(axis="x",
+                    labelbottom=False,
+                    labeltop=False,
+                    # labelsize=d_plot["fontsize_ticks"]
+                    )
+    ax2.set_xticks(t_xticks)
+    ax2.set_xticklabels(xticks_labels)
+    ax2.set_xlabel(d_plot["xlabel"], fontsize=d_plot["fontsize_labels"])
     ax2.set_ylabel(d_plot["voltagelabel"], fontsize=d_plot["fontsize_labels"])
-    ax2.xaxis.set_major_locator(ticker.MultipleLocator(base_time))
-    ax2.xaxis.set_minor_locator(ticker.MultipleLocator(base_time / d_plot["minor_tick_index_echem"]))
-    ax2.yaxis.set_major_locator(ticker.MultipleLocator(base_voltage))
-    ax2.yaxis.set_minor_locator(ticker.MultipleLocator(base_voltage / d_plot["minor_tick_index_echem"]))
+    ax2.xaxis.set_tick_params(labelsize=d_plot["fontsize_ticks"])
+    ax2.yaxis.set_tick_params(labelsize=d_plot["fontsize_ticks"])
+    if not isinstance(VLINES_ECHEM, type(None)):
+        ax21.axvline(x=0.987*t_changes[0], ls="--", c="k", lw=2, zorder=1)
+        ax21.axvline(x=0.9925*t_changes[1], ls="--", c="k", lw=2, zorder=1)
+        # for t in t_changes:
+        #     ax11.axvline(x=t, ls="--", c="k", lw=2, zorder=1)
+    # scan_time = np.array([i * (time_range / (scatt_array.shape[1] - 1))
+    #                       for i in range(scatt_array.shape[1])])
     for p in output_paths:
         plt.savefig(p / f"xy_overview_stack_echem.{p.name}",
                     bbox_inches="tight")
@@ -406,19 +488,20 @@ def main():
               f"the program.")
         sys.exit()
     print(f"{80*'-'}\nStacking xy data for upper plot...")
-    x_upper, y_upper = xy_stack(data_upper_files)
+    d_upper = xy_stack(data_upper_files)
     print(f"{80*'-'}\nStacking xy data for lower plot...")
-    x_lower, y_lower = xy_stack(data_lower_files)
+    d_lower = xy_stack(data_lower_files)
     echem_file = data_echem_files[0]
     print(f"{80*'-'}\nCollecting electrochemical data...\n\t{echem_file.name}")
-    time_voltage = np.loadtxt(echem_file)
-    data_dict = dict(x_upper = x_upper,
-                     y_upper = y_upper,
-                     x_lower = x_lower,
-                     y_lower = y_lower,
-                     time = time_voltage[:,0],
-                     voltage = time_voltage[:,1]
-                     )
+    d_echem = echem_load(echem_file)
+    d_echem = x_from_dict_calculate(d_echem)
+    # data_dict = dict(x_upper = x_upper,
+    #                  y_upper = y_upper,
+    #                  x_lower = x_lower,
+    #                  y_lower = y_lower,
+    #                  time = time_voltage[:,0],
+    #                  voltage = time_voltage[:,1]
+    #                  )
     output_paths = [Path.cwd() / "pdf",
                     Path.cwd() / "png",
                     Path.cwd() / "svg"]
@@ -426,7 +509,7 @@ def main():
         if not p.exists():
             p.mkdir()
     print(f"{80*'-'}\nMaking plot with stacked overview plots and echem...")
-    xy_overview_stack_echem(data_dict, PLOT_DICT, output_paths)
+    xy_overview_stack_echem(d_upper, d_lower, d_echem, PLOT_DICT, output_paths)
     output_folders = [p.name for p in output_paths]
     print(f"Done plotting.\n{80*'-'}\nPlease see the {output_folders} folders.")
 
