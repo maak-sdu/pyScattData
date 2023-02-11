26e32a25ccc76f0e7fd891d1ac0d57a70b5dd795
Martin Aaskov Karlsen <au714043@uni.au.dk>
2023-02-11 08:08:43 +0100

    using pathlib paths for plot files

--- iq_sq_fq_gr_plot/example/iq_sq_fq_gr_plot.py
+++ iq_sq_fq_gr_plot/example/iq_sq_fq_gr_plot.py
@@ -3,46 +3,60 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 from diffpy.utils.parsers.loaddata import loadData
+# from bg_mpl_stylesheet.bg_mpl_stylesheet import bg_mpl_style
 
-DPI = 300
+DPI = 600
 FIGSIZE = (8,4)
 FONTSIZE = 12
 LINEWIDTH = 1
-COLORS = dict(bg_blue='#0B3C5D', bg_red='#B82601', bg_green='#1c6b0a',
-              bg_lightblue='#328CC1', bg_darkblue='#062F4F', bg_yellow='#D9B310',
-              bg_darkred='#984B43', bg_bordeaux='#76323F', bg_olivegreen='#626E60',
-              bg_yellowgrey='#AB987A', bg_brownorange='#C09F80')
+COLORS = dict(bg_blue='#0B3C5D',
+              bg_red='#B82601',
+              bg_green='#1c6b0a',
+              bg_lightblue='#328CC1',
+              bg_darkblue='#062F4F',
+              bg_yellow='#D9B310',
+              bg_darkred='#984B43',
+              bg_bordeaux='#76323F',
+              bg_olivegreen='#626E60',
+              bg_yellowgrey='#AB987A',
+              bg_brownorange='#C09F80',
+              )
 
 
-def iq_sq_fq_gr_plotter(datafiles):
+def iq_sq_fq_gr_plotter(datafiles, output_paths):
     d = {}
     for f in datafiles:
         xy = loadData(f)
         d[str(f.suffix).split('.')[-1]] = xy
+    # plt.style.use(bg_mpl_style)
     fig, axs = plt.subplots(dpi=DPI, nrows=2, ncols=2, figsize=FIGSIZE)
     axs[0,0].plot(d['iq'][:,0], d['iq'][:,1], lw=LINEWIDTH, c=COLORS['bg_lightblue'])
     axs[0,0].set_xlabel(r'$Q$ $[\mathrm{\AA}^{-1}]$')
     axs[0,0].set_ylabel(r'$I$ $[\mathrm{a.u.}]$')
     axs[0,0].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     axs[0,0].set_xlim(min(d['iq'][:,0]), max(d['iq'][:,0]))
+    # axs[0,0].minorticks_on()
     axs[0,1].plot(d['sq'][:,0], d['sq'][:,1], lw=LINEWIDTH, c=COLORS['bg_blue'])
     axs[0,1].set_xlabel(r'$Q$ $[\mathrm{\AA}^{-1}]$')
     axs[0,1].set_ylabel(r'$S$ $[\mathrm{a.u.}]$')
     axs[0,1].set_xlim(min(d['sq'][:,0]), max(d['sq'][:,0]))
+    # axs[0,1].minorticks_on()
     axs[1,1].plot(d['fq'][:,0], d['fq'][:,1], lw=LINEWIDTH, c=COLORS['bg_green'])
     axs[1,1].set_xlabel(r'$Q$ $[\mathrm{\AA}^{-1}]$')
     axs[1,1].set_ylabel(r'$F$ $[\mathrm{\AA}^{-1}]$')
     axs[1,1].set_xlim(min(d['fq'][:,0]), max(d['fq'][:,0]))
+    # axs[1,1].minorticks_on()
     axs[1,0].plot(d['gr'][:,0], d['gr'][:,1], lw=LINEWIDTH, c=COLORS['bg_red'])
     axs[1,0].set_xlabel(r'$r$ $[\mathrm{\AA}]$')
     axs[1,0].set_ylabel(r'$G$ $[\mathrm{\AA}^{-2}]$')
     axs[1,0].set_xlim(min(d['gr'][:,0]), max(d['gr'][:,0]))
+    # axs[1,0].minorticks_on()
     plt.figtext(0.485, 0.725, r'$\rightarrow$', fontsize=20)
     plt.figtext(0.75, 0.46, r'$\downarrow$', fontsize=20)
     plt.figtext(0.485, 0.285, r'$\leftarrow$', fontsize=20)
     fig.tight_layout(pad=2)
-    plt.savefig(f"png/iq_fq_sq_gr.png", bbox_inches='tight')
-    plt.savefig(f"pdf/iq_fq_sq_gr.pdf", bbox_inches='tight')
+    for p in output_paths:
+        plt.savefig(f"{p.name}/iq_fq_sq_gr.{p.name}", bbox_inches='tight')
     plt.close()
 
     return None
@@ -69,12 +83,14 @@ def main():
                 \nPlease place them there and rerun the code.\
                 \n{80*'-'}")
         sys.exit()
-    folders = ['png', 'pdf']
-    for folder in folders:
-        if not (Path.cwd() / folder).exists():
-            (Path.cwd() / folder).mkdir()
+    png_path, pdf_path = Path.cwd() / "png", Path.cwd() / "pdf"
+    svg_path = Path.cwd() / "svg"
+    plot_paths = [png_path, pdf_path, svg_path]
+    for p in plot_paths:
+        if not p.exists():
+            p.mkdir()
     print(f"{80*'-'}\nPlotting...")
-    iq_sq_fq_gr_plotter(datafiles)
+    iq_sq_fq_gr_plotter(datafiles, plot_paths)
     print(f"Done plotting.\n{80*'-'}\
             \nPlease see the 'pdf' and 'png' folders.")
 
